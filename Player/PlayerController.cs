8a20dd51b7ead26ee67065f59b703597cee761ff
Isaac <156547@chrome.escg.ac.uk>
2022-04-14 16:26:43 +0100

    movement

--- Player/PlayerController.cs
+++ Player/PlayerController.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Fields
+    CharacterController controller;
+
+    // Mono methods
     void Start()
     {
-        
-    }
+        controller = gameObject.AddComponent<CharacterController>();
 
-    // Update is called once per frame
+    }
     void Update()
     {
-        
+        HandleInput();
+        Debug.Log(transform.position);
     }
+
+    // Methods
+    // Get player input
+    void HandleInput(){
+        // WASD
+        if(Input.GetKeyDown("w")){
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        }
+        if(Input.GetKeyDown("a")){
+            Debug.Log("a");
+        }
+        if(Input.GetKeyDown("s")){
+            Debug.Log("s");
+        }
+        if(Input.GetKeyDown("d")){
+            Debug.Log("d");
+        }
+    }
+    // Move player 
 }
